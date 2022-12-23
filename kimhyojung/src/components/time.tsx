0c1f4d5911b780c1dfc234704d407b78f7c4af37
khj0426 <706shin1728@naver.com>
2022-12-23 09:36:44 +0900

    feat:배경에 시간 추가

---
+++ kimhyojung/src/components/time.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+const StyledTime = styled.section`
+  font-size: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  margin-top: 10%;
+`;
+
+const StyledPhrases = styled.section`
+  font-size: 2.5rem;
+  color: white;
+  margin-top: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export default function CurrentTime() {
+  const currentDate = new Date();
+  const currentHour = currentDate.getHours().toString();
+  const currentMinute = currentDate.getMinutes().toString();
+
+  return (
+    <>
+      <StyledTime>
+        {Number(currentHour) < Number(10) ? '0' + currentHour : currentHour} :
+        {Number(currentMinute) < Number(10)
+          ? '0' + currentMinute
+          : currentMinute}
+      </StyledTime>
+      <StyledPhrases>Have a nice day</StyledPhrases>
+    </>
+  );
+}
