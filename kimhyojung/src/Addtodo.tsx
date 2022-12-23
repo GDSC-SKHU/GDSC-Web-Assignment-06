b4383d416160c110fb01862f4943a5091c96aac4
khj0426 <706shin1728@naver.com>
2022-12-23 10:06:14 +0900

    style:Todo추가하는 입력칸에 스타일 설정

--- kimhyojung/src/Addtodo.tsx
+++ kimhyojung/src/Addtodo.tsx
@@ -1,13 +1,38 @@
 import useaddTodo from './components/hooks/usaaddTodo';
-
+import styled from 'styled-components';
 export default function Addtodo() {
   const { onclick, onsubmit, newTodoRef } = useaddTodo();
   return (
     <form onSubmit={onsubmit}>
-      <input ref={newTodoRef}></input>
-      <button type="submit" onClick={onclick}>
+      <StyledInput ref={newTodoRef}></StyledInput>
+      <StyledButton type="submit" onClick={onclick}>
         추가
-      </button>
+      </StyledButton>
     </form>
   );
 }
+
+const StyledInput = styled.input`
+  border: 1px solid #e0e0e0;
+  border-radius: 40px;
+  background: none;
+  margin: auto;
+  padding: 3px;
+  &:focus {
+    background: #e0e0e0;
+    color: black;
+    font-weight: 700;
+    border: none;
+  }
+`;
+
+const StyledButton = styled.button`
+  background: none;
+  border: 1px solid #e0e0e0;
+  cursor: pointer;
+  padding: 3px;
+  &:hover {
+    background: #e0e0e0;
+    color: black;
+  }
+`;
