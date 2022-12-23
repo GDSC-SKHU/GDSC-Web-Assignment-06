dbc60ddb403d1eaa09c43dc3db60f4aa929c2c8b
baeksehee <tpgmlqor@gmail.com>
2022-12-23 16:39:46 +0900

    feat: todolist 기능 추가

--- baeksehee/src/libs/api.ts
+++ baeksehee/src/libs/api.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const instance = axios.create({
     baseURL: "https://api.airtable.com/v0/appcPS8MQisCxTMK3/",
-    headers:{Authoriztion: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`},
+    headers:{ Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`},
 });
