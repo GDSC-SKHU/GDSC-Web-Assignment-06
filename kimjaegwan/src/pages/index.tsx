9ccf45af38df0afbe9d0b6cea2339321be8d4b76
jaegwans <skhu201811140@gmail.com>
2022-12-23 14:14:03 +0900

    feat: 수정 기능 구현

--- kimjaegwan/src/pages/index.tsx
+++ kimjaegwan/src/pages/index.tsx
@@ -43,7 +43,30 @@ const Home = () => {
     };
 
     //수정 기능 구현
-    const _onUpdate = (e: React.MouseEvent<HTMLButtonElement>) => {};
+    const _onUpdate = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        if (newTodo === '') {
+            alert('내용을 입력하고 수정하세요.');
+            return;
+        }
+
+        instance
+            .patch('/todos', {
+                records: [
+                    {
+                        id: e.currentTarget.id,
+                        fields: {
+                            Name: newTodo,
+                        },
+                    },
+                ],
+            })
+            .then(() => {
+                saveTodos();
+                // ? setNewTodo(''); 왜 여기에 들어가면 작동을 하지 않는지 이해가가지 않음
+            });
+        setNewTodo('');
+    };
 
     return (
         <>
@@ -53,12 +76,14 @@ const Home = () => {
                 {todos.map((data) => (
                     <StyledTodo key={data.id}>
                         <p key={data.id}>{data.fields.Name} </p>
-                        <button onClick={_onDelete} id={data.id}>
-                            삭제
-                        </button>
-                        <button onClick={_onDelete} id={data.id}>
-                            수정
-                        </button>
+                        <div>
+                            <button onClick={_onDelete} id={data.id}>
+                                삭제
+                            </button>
+                            <button onClick={_onUpdate} id={data.id}>
+                                수정
+                            </button>
+                        </div>
                     </StyledTodo>
                 ))}
 
@@ -78,11 +103,22 @@ const Home = () => {
 export default Home;
 
 const StyledHomeBox = styled.div`
-    border: 3px solid black;
+    border: 3px solid #81818148;
     width: 600px;
     margin-top: 1.875rem;
+    button {
+        all: unset;
+        border: 1px solid black;
+        margin: 3px;
+        padding: 1px;
+        &:hover {
+            background-color: #81818148;
+        }
+    }
 `;
 const StyledTodo = styled.div`
     display: flex;
     flex-direction: row;
+    justify-content: space-between;
+    border-bottom: 3px solid #81818148;
 `;
